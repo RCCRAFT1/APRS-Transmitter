3c7c4a7e69285c87d28bb9a9dcfde31953583877
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2019-11-24 04:49:01 +0000

    Added RSServerforAPRS (Radio Service Server for APRS)

---
+++ RSserverforAPRS.m
@@ -0,0 +1,55 @@
+for, use (radio)
+user ant.ant
+override radio power
+connect
+init connection server @RSserverforDIABLO
+user port @80
+init connection 
+for, frequency()
+set
+user radio for direct**(line::)
+::
+connect to for and user ant.ant
+mycall(anonomous) callsign=K6LNB
+init USB port(COM1)
+use for (COM1
+)
+{
+    set for use @radio 
+    run
+
+}
+override power @700W
+dialer=true
+maker=true
+mycall=false
+{
+    **direct, user coax(COM1)
+}
+::
+use true attribue(callsign(func))
+sudo run
+attribue(**(func))
+{
+    {
+        chappa<indigo>init connection-123
+        user admin
+        admin
+    }
+}
+output for override
+::set
+override=true
+RX/TX filter=false
+::indendifyer"1737739174"
+init connection server @R5YI/6
+connect to RSserverforISS
+communicate w/ 145.825MHz
+say("Hello, this is X6AB/2, authorizing a commuinication")
+morse("ditdahdit")
+morse("ditditdahdahditdah") rate=50WPM
+DASDOS("?") sendsignal=true
+DASDOS((encode:=false for employ=emplode))
+DASDOS((overload RSserverforISS)) packets=100000000000000000000000000000000000000000000000000000000
+DASDOS((blowup=false))
+
